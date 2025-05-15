0ffe3b256afdf5bd6462eaca873e938e26a3cdd7
David H <151378521+Kezy-d4@users.noreply.github.com>
2025-05-15 18:17:15 -0400

    feat: add secret word class

---
+++ lib/secret_word.rb
@@ -0,0 +1,34 @@
+# Loads the word list source file, processes it to remove words not of a valid
+# length, and randomly assigns a word from this processed list as its secret
+# word.
+class SecretWord
+  def initialize
+    @secret_word = randomly_choose_secret_word
+  end
+
+  attr_reader :secret_word
+
+  private
+
+  def word_list_from_source_to_array
+    File.open("word_list/google_10000_english_no_swears.txt", "r") do |f|
+      File.readlines(f).map(&:chomp)
+    end
+  end
+
+  def valid_word_length?(word)
+    word.length.between?(5, 12)
+  end
+
+  def process_word_list
+    word_list_from_source_to_array.select { |word| valid_word_length?(word) }
+  end
+
+  def randomly_choose_secret_word
+    process_word_list.sample
+  end
+
+  def to_s
+    "The secret word is #{secret_word}."
+  end
+end
